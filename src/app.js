f1d600011026bee02c517d0b30b4e641df5036f9
Jacobo Tapia <jacob.et.cetera@gmail.com>
2018-09-30 16:14:52 -0500

    Init

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<p>Hello</p>, document.getElementById("app"));
